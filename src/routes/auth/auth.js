04f28be625a0360ecab0a8f13b39a000a8e3ff5d
truongxuantran <truongnhthtb@gmail.com>
2019-07-15 17:01:45 +0700

    sign in api

--- src/routes/auth/auth.js
+++ src/routes/auth/auth.js
@@ -5,29 +5,30 @@ const { generatePassword, decryptPassword, responseError } = require('../../util
 //sign up
 app.post('/sign-up', (req, res, next) => {
     req.assert('email', 'A valid Email is required').isEmail();
-    req.assert('password', 'Password cannot empty').is;
+    req.assert('password', 'Password cannot empty').notEmpty();
     const errors = req.validationErrors();
-    if(!errors){
+    if (!errors) {
         const user = {
             email: req.sanitize('email').escape().trim(),
             password: req.sanitize('password').escape().trim(),
         }
-        generatePassword(user.password,(hash) => {
+        generatePassword(user.password, (hash) => {
             req.getConnection((error, conn) => {
-                conn.query("SELECT * FROM user WHERE email = '" +  user.email + "'", (err, rows, fields) => {
-                    if(err) res.send(responseError(false, null, err));
-                    if(rows.length > 0){
+                const query  = "SELECT * FROM user WHERE email = '" + user.email + "'";
+                conn.query(query, (err, rows, fields) => {
+                    if (err) res.send(responseError(false, null, err));
+                    if (rows.length > 0) {
                         res.send(responseError(false, null, 'Your email register has exited. Please use other email to register'));
-                    }else{
+                    } else {
                         conn.query("INSERT INTO user (email, password) VALUES ('" + user.email + "','" + hash + "')", (err, results) => {
-                            if(err) throw err;
+                            if (err) throw err;
                             res.send(responseError(true, null, 'Register success'))
                         })
                     }
                 })
             })
         });
-    }else {
+    } else {
         res.send(responseError(false, null, errors));
     }
 })
@@ -36,7 +37,35 @@ app.post('/sign-up', (req, res, next) => {
 //sign in
 
 app.post('/sign-in', (req, res, next) => {
-    
+    req.assert('email', 'A valid Email is required').isEmail();
+    req.assert('password', 'Password cannot empty').notEmpty();
+    const errors = req.validationErrors();
+    if (!errors) {
+        const user = {
+            email: req.sanitize('email').escape().trim(),
+            password: req.sanitize('password').escape().trim(),
+        }
+        req.getConnection((error, conn) => {
+            const query = "SELECT * FROM user WHERE email = '" + user.email + "'";
+            conn.query(query, (err, rows, fields) => {
+                if (err) res.send(responseError(false, null, err));
+                if (rows.length > 0) {
+                    const hashPass = rows[0].password;
+                    decryptPassword(user.password,hashPass, (match) => {
+                        if(match){
+                            res.send(responseError(true, null, 'Login success'))
+                        }else{
+                            res.send(responseError(false, null, 'Password wrong'))
+                        }
+                    } )
+                } else {
+                    res.send(responseError(false, null, 'Email wrong'))
+                }
+            })
+        })
+    } else {
+        res.send(responseError(false, null, errors));
+    }
 })
 
 module.exports = app;
